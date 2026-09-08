ad96d3fa81342e9a0e5e8a07fa021cdcadb00443
Fructueux ESSOU <narufruc@gmail.com>
2026-09-08 23:38:53 +0100

    Eh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/tuto-docker/', // 👈 Indispensable pour GitHub Pages
   plugins: [react()],
 })
